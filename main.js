feccdc9fbd11c0db64e3d8a99fd626f58b10629d
Federica Alfano <alfanofederica95@gmail.com>
2018-01-07 14:40:08 +0100

    animations and bug fixes

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+//BG IMAGE
+
+/*
+const icons = document.querySelectorAll('.backgroundImage');
+
+
+    icons.addEventListener('mousemove',function () {
+
+        //code here
+    });
+
+
+*/
+
+// SCROLL
+
 const scrollTarget = document.querySelector('.scrollTarget');
 const sections = document.querySelectorAll('.scrollSection');
 let sectionCounter = 0;
@@ -5,29 +21,30 @@ let sectionIsAnimating = false;
 
 window.addEventListener('wheel', function () {
 
-    event.preventDefault();
-    if(sectionIsAnimating === true){
-        return;
+    if(window.innerWidth > 400){
+        event.preventDefault();
+        if(sectionIsAnimating === true){
+            return;
+        }
+
+        let newSectionCounter = sectionCounter;
+        newSectionCounter += event.wheelDelta < 0 ? 1 : -1;
+        newSectionCounter = Math.min(Math.max(0, newSectionCounter), sections.length-1);
+
+
+        if(newSectionCounter !== sectionCounter){
+            sectionCounter = newSectionCounter;
+            scrollTarget.style = '--currentSection: ' + sectionCounter;
+            sectionIsAnimating = true;
+
+            setTimeout(function () {
+                sectionIsAnimating = false;
+            }, 1000);
+        }
     }
-
-    let newSectionCounter = sectionCounter;
-    newSectionCounter += event.wheelDelta < 0 ? 1 : -1;
-    newSectionCounter = Math.min(Math.max(0, newSectionCounter), sections.length);
-
-    console.log('not animating');
-
-    if(newSectionCounter !== sectionCounter){
-        sectionCounter = newSectionCounter;
-        scrollTarget.style = '--currentSection: ' + sectionCounter;
-        sectionIsAnimating = true;
-
-        setTimeout(function () {
-           sectionIsAnimating = false;
-        }, 1000);
-    }
-
-
 
 
 });
+
+
 
