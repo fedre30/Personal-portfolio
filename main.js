901d89770e25eff174fad38b21f2f01ba773ff7c
Federica Alfano <alfanofederica95@gmail.com>
2018-01-14 13:40:11 +0100

    animoji and animations

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //BG IMAGE
 const rotateCoeff = 15;
 
-const icons = document.querySelector('.backgroundImage');
+const icons = document.querySelector('.introduction');
 let screenWidth = window.innerWidth;
 let screenHeight = window.innerHeight;
 
@@ -19,6 +19,7 @@ icons.addEventListener('mousemove',function (ev) {
 
 
 
+
 // SCROLL
 
 const scrollTarget = document.querySelector('.scrollTarget');
@@ -26,10 +27,55 @@ const sections = document.querySelectorAll('.scrollSection');
 let sectionCounter = 0;
 let sectionIsAnimating = false;
 const dots = document.querySelectorAll('.menu-item');
+const projectTitle = document.querySelector('.projectTitle');
 
 function refreshSectionDots(currentSection) {
     for(let i = 0; i < dots.length; i++){
         dots[i].style.backgroundColor = i === currentSection ? 'white' : 'transparent';
+
+    }
+}
+
+function triggerSectionAnimations() {
+
+    const sections = [
+        document.querySelector('.introduction'),
+        document.querySelector('.workflow'),
+        document.querySelector('.project-1'),
+        document.querySelector('.project-2'),
+        document.querySelector('.project-3'),
+        document.querySelector('.about')
+
+    ];
+
+    for(let i =0; i < sections.length; i++){
+        if(i === sectionCounter){
+            sections[i].classList.add('visible');
+
+        }
+        else{
+            sections[i].classList.remove('visible');
+        }
+    }
+
+
+}
+
+
+
+function setCurrentSection(section) {
+    if(section !== sectionCounter) {
+        sectionCounter = section;
+        scrollTarget.style = '--currentSection: ' + section;
+
+        sectionIsAnimating = true;
+
+        setTimeout(function () {
+            sectionIsAnimating = false;
+        }, 1000);
+
+        triggerSectionAnimations();
+        refreshSectionDots(section);
     }
 }
 
@@ -46,23 +92,43 @@ window.addEventListener('wheel', function () {
         newSectionCounter = Math.min(Math.max(0, newSectionCounter), sections.length - 2);
 
 
-        if(newSectionCounter !== sectionCounter){
-            sectionCounter = newSectionCounter;
-            scrollTarget.style = '--currentSection: ' + sectionCounter;
-            sectionIsAnimating = true;
-
-            setTimeout(function () {
-                sectionIsAnimating = false;
-            }, 1000);
-
-            refreshSectionDots(newSectionCounter);
-        }
+        setCurrentSection(newSectionCounter);
     }
 
 
 });
 
+for(let i = 0; i < dots.length; i++){
+    dots[i].addEventListener('click', function () {
+        console.log(i);
+        setCurrentSection(i);
+    });
+}
+
 refreshSectionDots(sectionCounter);
+
+
+//MENU
+
+const introDot = document.querySelector('.menuIntroduction');
+const introLabel = document.querySelector('.introLabel');
+
+for(let i = 0; i < dots.length; i++){
+    const selector = dots[i].dataset.tooltipId;
+    const tooltip = document.querySelector(selector);
+
+    dots[i].addEventListener('mouseenter', function () {
+        tooltip.classList.add('visible');
+    });
+
+    dots[i].addEventListener('mouseleave', function () {
+        tooltip.classList.remove('visible');
+    });
+}
+
+
+
+
 
 
 // CONTACT FORM
@@ -106,6 +172,26 @@ document.querySelector(".send").addEventListener("click", function () {
 });
 */
 
+/*
+
+const mail = document.querySelector('.mail_botprotection');
+
+mail.each(function(i)
+    {
+        this.html("<a href=\"" + "mail" + "to:" + $(this).data("id") + "@" + $(this).data("server") + "\">" + '<span class="fa fa-envelope" aria-hidden="true"></span>' + "</a>");
+    }
+);
+});
 
 
 
+
+
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='https://www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+
+*/
